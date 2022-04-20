48cf6d8a584b14f01a6e3d5805740e331f3efe77
danieltheil <danieltheil>
2022-04-21 01:17:17 +0200

    Added Mobile/small Screen size support

--- src/App.js
+++ src/App.js
@@ -7,31 +7,36 @@ import MobileAlcCardView from './Components/MobileViews/MobileAlcCardView';
 import MobileMixCardView from './Components/MobileViews/MobileMixCardView';
 import MixCardView from './Components/DesktopViews/MixCardView';
 
+import useWindowDimensions from './utils/WindowDimensionsGrabber';
 import viewStates from './utils/ViewStates';
 import { useState } from 'react';
 import { isMobile } from 'react-device-detect';
 
 function App() {
   document.body.style.backgroundColor = colors.background;
+  
   const [viewState, setViewState] = useState(viewStates.alcoholView);
+  const { width } = useWindowDimensions();
+  const MAX_WIDTH = 1200;
 
   return (
     <div className="App h-screen">
       <NavBar />
       <div className='content-container grid grid-cols-12 h-full w-full' style={{backgroundColor: colors.background}}>
-        { isMobile ?
+        { isMobile || width < MAX_WIDTH ?
           null :
           (<SideBar cols="col-span-1" viewState={viewState} setViewState={setViewState}/>) 
         }
+
         <div className="card-container grid grid-cols-9 grid-rows-10 col-span-11 grid-flow-row">
-          { isMobile ? (
+          { isMobile || width < MAX_WIDTH ? (
               viewState === viewStates.alcoholView ? 
-              (<MobileAlcCardView isMobile={isMobile} setViewState={setViewState}/>) :
-              (<MobileMixCardView isMobile={isMobile} setViewState={setViewState}/>)
+                (<MobileAlcCardView isMobile={ isMobile || width < MAX_WIDTH } setViewState={setViewState}/>) :
+                (<MobileMixCardView isMobile={ isMobile || width < MAX_WIDTH } setViewState={setViewState}/>)
             ) : (
               viewState === viewStates.alcoholView ? 
-              (<AlcCardView  setViewState={setViewState}/>) :
-              (<MixCardView  setViewState={setViewState} />)
+                (<AlcCardView  setViewState={setViewState}/>) :
+                (<MixCardView  setViewState={setViewState} />)
             )
           }
         </div>
